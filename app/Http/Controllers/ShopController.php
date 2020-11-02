325a950d21c5a6a7eade3905826bf53571e3cef1
Salwan-Aldulaimi <salwan@holygrow.nl>
2020-11-03 00:41:30 +0100

    style in welcome page

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -9,9 +9,9 @@ class ShopController extends Controller
 {
     public function index()
     {
-     //   $product = Product::all();
-     $products = Product::latest()->paginate(10);
-       return view('shop.index', compact('products'));
+        //   $product = Product::all();
+        $products = Product::latest()->paginate(10);
+        return view('shop.index', compact('products'));
     }
 
     public function create()
@@ -22,13 +22,13 @@ class ShopController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name'=>'required',
-            'price'=>'required'
+            'name' => 'required',
+            'price' => 'required'
         ]);
 
         $products = Product::create($request->all());
-         return redirect()->route('shop.index')
-         ->with('success','product added successflly');
+        return redirect()->route('shop.index')
+            ->with('success', 'product added successflly');
     }
 
     public function show(Product $product)
@@ -36,21 +36,21 @@ class ShopController extends Controller
         return view('shop.show', compact('product'));
     }
 
-    public function edit(Product $product,$id)
+    public function edit(Product $product, $id)
     {
         $product = Product::find($id);
-        return view('shop.edit', compact('product','id'));
+        return view('shop.edit', compact('product', 'id'));
     }
     public function update(Request $request, Product $product, $id)
     {
         $product = Product::find($id);
-        $product->name        = $request->input('name');
-        $product->price      = $request->input('price');
-        $product->detail  = $request->input('detail');
+        $product->name      = $request->input('name');
+        $product->price     = $request->input('price');
+        $product->detail    = $request->input('detail');
 
         $product->save();
-         return redirect()->route('shop.index')
-         ->with('success','product updated successflly');
+        return redirect()->route('shop.index')
+            ->with('success', 'product updated successflly');
     }
 
     public function destroy($id)
@@ -58,6 +58,6 @@ class ShopController extends Controller
         $product = Product::find($id);
         $product->delete();
         return redirect()->back()
-        ->with('success_delete','product deleted successflly');
+            ->with('success_delete', 'product deleted successflly');
     }
 }
