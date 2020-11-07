88c755b5dcfbf1d93b0b327f4cca3eb525e92b08
Salwan-Aldulaimi <salwan@holygrow.nl>
2020-11-07 21:08:51 +0100

    Profile doen

---
+++ database/migrations/2020_11_07_184257_create_profile_users.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProfileUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profile_users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('telefoon');
+            $table->longText('bio');
+            $table->longText('link');
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('user')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profile_users');
+    }
+}
