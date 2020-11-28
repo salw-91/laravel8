509b941fa1a837fbce9a0ddad7b18315a28d7f9a
Salwan-Aldulaimi <salwan@holygrow.nl>
2020-11-28 23:12:46 +0100

    api 2/8

--- app/Http/Controllers/api/BaseController.php
+++ app/Http/Controllers/api/BaseController.php
@@ -6,5 +6,25 @@ use Illuminate\Http\Request;
 
 class BaseController extends Controller
 {
-    //
+    public function sendResponse($result, $message)
+    {
+        $response = [
+            'success' => true,
+            'data' => $result,
+            'message' => $message
+                    ];
+        return response()->json($response , 200);
+    }
+
+    public function sendError($error, $errorMessage=[], $code = 404)
+    {
+        $response = [
+            'success' => true,
+            'data' => $result
+    ];
+    if (!empty ($errorMessage)) {
+        $response['data'] = $errorMessage;
+    }
+    return response()->json($response, $code);
+    }
 }
